6113f65e7fc3e0bf36d4795d44aa937a113a6263
yihuaf <fang.yihua.eric@gmail.com>
2019-07-29 15:26:04 -0700

    Add kontain log store implementation
    no ops for now

---
+++ core/invoker/src/main/scala/org/apache/openwhisk/core/containerpool/kontain/KontainLogStore.scala
@@ -0,0 +1,53 @@
+package org.apache.openwhisk.core.containerpool.kontain
+
+import akka.actor.ActorSystem
+import org.apache.openwhisk.common.TransactionId
+import org.apache.openwhisk.core.containerpool.Container
+import org.apache.openwhisk.core.containerpool.logging.{LogStore, LogStoreProvider}
+import org.apache.openwhisk.core.database.UserContext
+import org.apache.openwhisk.core.entity.{ActivationLogs, ExecutableWhiskAction, Identity, WhiskActivation}
+
+import scala.concurrent.Future
+
+object KontainLogStoreProvide extends LogStoreProvider {
+  override def instance(actorSystem: ActorSystem): LogStore = new KontainLogStore()
+}
+
+class KontainLogStore extends LogStore {
+
+  /** Additional parameters to pass to container creation */
+  override def containerParameters: Map[String, Set[String]] = Map()
+
+  /**
+   * Collect logs after the activation has finished.
+   *
+   * This method is called after an activation has finished. The logs gathered here are stored along the activation
+   * record in the database.
+   *
+   * @param transid    transaction the activation ran in
+   * @param user       the user who ran the activation
+   * @param activation the activation record
+   * @param container  container used by the activation
+   * @param action     action that was activated
+   * @return logs for the given activation
+   */
+  override def collectLogs(transid: TransactionId,
+                           user: Identity,
+                           activation: WhiskActivation,
+                           container: Container,
+                           action: ExecutableWhiskAction): Future[ActivationLogs] = {
+    Future.successful(ActivationLogs(Seq("kontain not implemented").toVector))
+  }
+
+  /**
+   * Fetch relevant logs for the given activation from the store.
+   *
+   * This method is called when a user requests logs via the API.
+   *
+   * @param activation activation to fetch the logs for
+   * @return the relevant logs
+   */
+  override def fetchLogs(activation: WhiskActivation, context: UserContext): Future[ActivationLogs] = {
+    Future.successful(activation.logs)
+  }
+}
