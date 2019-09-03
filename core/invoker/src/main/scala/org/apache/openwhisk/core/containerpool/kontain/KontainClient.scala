84f35669cfd97789e41b022be2235e482393fa90
yihuaf <fang.yihua.eric@gmail.com>
2019-09-02 23:37:24 -0700

    Use kontain instead of runk

--- core/invoker/src/main/scala/org/apache/openwhisk/core/containerpool/kontain/KontainClient.scala
+++ core/invoker/src/main/scala/org/apache/openwhisk/core/containerpool/kontain/KontainClient.scala
@@ -24,9 +24,10 @@ trait KontainApi {
   def removeAllContainers()(implicit transactionId: TransactionId): Future[Unit]
 }
 
-class KontainClient()(override implicit val executionContext: ExecutionContext,
-                      implicit val as: ActorSystem,
-                      implicit val log: Logging)
+class KontainClient()(
+  override implicit val executionContext: ExecutionContext,
+  implicit val as: ActorSystem,
+  implicit val log: Logging)
     extends KontainApi
     with ProcessRunner {
 
@@ -43,35 +44,38 @@ class KontainClient()(override implicit val executionContext: ExecutionContext,
 
   override def run(image: String, name: String)(port: Int)(implicit transid: TransactionId): Future[ContainerId] = {
     log.info(this, s"kontain run (image ${image}) (name ${name})")
-    runCmd("runk", Seq("run", name, "-b", image, "--args", port.toString), 5.seconds)
+    runCmd(
+      Seq("kontain", "container", "run", image, name, "--args", port.toString, "--debug"),
+      5.seconds,
+      "kontain_run")
       .map(_ => ContainerId(name))
   }
 
-  protected def runCmd(cmd: String, args: Seq[String], timeout: Duration)(
+  protected def runCmd(cmd: Seq[String], timeout: Duration, tag: String)(
     implicit transid: TransactionId): Future[String] = {
     val start = transid.started(
       this,
-      LoggingMarkers.INVOKER_KONTAIN_CMD(args.head),
+      LoggingMarkers.INVOKER_KONTAIN_CMD(tag),
       s"running ${cmd.mkString(" ")} (timeout: $timeout)",
       logLevel = InfoLevel)
-    executeProcess(Seq(cmd) ++ args, timeout).andThen {
+    executeProcess(cmd, timeout).andThen {
       case Success(_) => transid.finished(this, start)
       case Failure(pte: ProcessTimeoutException) =>
         transid.failed(this, start, pte.getMessage, ErrorLevel)
-        MetricEmitter.emitCounterMetric(LoggingMarkers.INVOKER_KONTAIN_CMD_TIMEOUT(args.head))
+        MetricEmitter.emitCounterMetric(LoggingMarkers.INVOKER_KONTAIN_CMD_TIMEOUT(tag))
       case Failure(t) => transid.failed(this, start, t.getMessage, ErrorLevel)
     }
   }
 
   override def rm(containerId: ContainerId)(implicit transid: TransactionId): Future[Unit] = {
     log.info(this, s"removing kontain ${containerId.asString}")
-    runCmd("runk", Seq("delete", "--force", containerId.asString), Duration.Inf)
+    runCmd(Seq("kontain", "container", "delete", containerId.asString), 5.seconds, "kontain_delete")
       .map(_ => ())
   }
 
   override def removeAllContainers()(implicit transactionId: TransactionId): Future[Unit] = {
     log.info(this, "remove all containers")
-    runCmd("runk", Seq("nuke"), 5.seconds).map(_ => ())
+    runCmd(Seq("kontain", "container", "nuke"), 5.seconds, "kontain_nuke").map(_ => ())
   }
 
 }
